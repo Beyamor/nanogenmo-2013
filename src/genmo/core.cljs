5951c5d10fcf3f8ad83d50fe628f9a833fb74f4d
beyamor <beyamor@gmail.com>
2013-11-09 13:51:12 -0800

    Started building a protagonist

--- src/genmo/core.cljs
+++ src/genmo/core.cljs
@@ -1,5 +1,12 @@
 (ns genmo.core
-  (:use [jayq.core :only [$]]))
+  (:use [jayq.core :only [$ text]])
+  (:require [genmo.data.names :as names]))
 
-($
-  (js/alert "hello"))
+(let [gender (rand-nth [:male :female])]
+  (def story
+    {:protagonist
+     {:gender gender
+      :name (rand-nth (names/all gender))}}))
+
+($ #(-> ($ :body)
+      (text (str story))))
