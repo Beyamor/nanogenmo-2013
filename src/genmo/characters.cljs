0eca69f92d029842b8246bdce3f97dac358e3701
beyamor <beyamor@gmail.com>
2013-11-09 15:08:06 -0800

    create multimethod

--- src/genmo/characters.cljs
+++ src/genmo/characters.cljs
@@ -1,17 +1,10 @@
 (ns genmo.characters
   (:require [genmo.data.names :as names]))
 
-(defn create-human
+(defmulti create :type)
+
+(defmethod create :human
   [constraints]
   (let [gender (rand-nth [:male :female])]
     {:gender gender
      :name (rand-nth (names/all gender))}))
-
-(def type-constructors
-  {:human create-human})
-
-(defn create
-  [constraints]
-  (if-let [constructor (-> constraints :type type-constructors)]
-    (constructor constraints)
-    (throw (js/Error. (str "No type constructor matches " (:type constraints))))))
