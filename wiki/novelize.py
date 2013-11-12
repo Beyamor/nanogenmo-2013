dc74e34496d0433e25fdfb99edbb5e6f35696a40
beyamor <beyamor@gmail.com>
2013-11-11 19:27:14 -0800

    Multiple passes to build up novel

--- wiki/novelize.py
+++ wiki/novelize.py
@@ -3,6 +3,7 @@ from random import choice
 import urllib2
 import re
 import nltk
+import time
 
 def constantly(value):
 	def f(**kwargs):
@@ -52,47 +53,55 @@ with open("authors", "r") as authors_file:
 	for link in authors_file.read().split("\n"):
 		author_links.append("http://en.wikipedia.org" + link)
 
-link		= choice(author_links)
-soup		= BeautifulSoup(urllib2.urlopen(link))
-content		= soup.find(id="mw-content-text")
+novel		= []
+novel_length	= 0
+while novel_length < 500:
+	link			= choice(author_links)
+	soup			= BeautifulSoup(urllib2.urlopen(link))
+	content			= soup.find(id="mw-content-text")
+	section_sentences	= {}
+	current_section		= None
+	for content_item in content.children:
+		if content_item.name == "h2":
+			section_heading = content_item.find(class_="mw-headline")
 
-print link
+			if section_heading:
+				current_section = section_heading.get_text()
 
-section_sentences	= {}
-current_section		= None
-for content_item in content.children:
-	if content_item.name == "h2":
-		section_heading = content_item.find(class_="mw-headline")
+		elif content_item.name == "p" and is_interesting_section(current_section):
+			if not current_section in section_sentences:
+				section_sentences[current_section] = []
+			exiting_sentences = section_sentences[current_section]
 
-		if section_heading:
-			current_section = section_heading.get_text()
+			sentences = nltk.tokenize.sent_tokenize(content_item.get_text())
+			for sentence in sentences:
+				sentence_without_citations	= re.sub("\[\d+\]", "", sentence)
+				sentence_without_parens		= re.sub("\(.*?\)", "", sentence_without_citations)
+				stripped_sentence		= sentence_without_citations.strip()
 
-	elif content_item.name == "p" and is_interesting_section(current_section):
-		if not current_section in section_sentences:
-			section_sentences[current_section] = []
-		exiting_sentences = section_sentences[current_section]
+				if len(stripped_sentence) > 0:
+					exiting_sentences.append(stripped_sentence)
 
-		sentences = nltk.tokenize.sent_tokenize(content_item.get_text())
+	interesting_content = []
+	for section, sentences in section_sentences.items():
+		orderer	= section_sentence_orderer(section)
+		index	= 1.0
+
 		for sentence in sentences:
-			sentence_without_citations	= re.sub("\[\d+\]", "", sentence)
-			sentence_without_parens		= re.sub("\(.*?\)", "", sentence_without_citations)
-			stripped_sentence		= sentence_without_citations.strip()
-
-			if len(stripped_sentence) > 0:
-				exiting_sentences.append(stripped_sentence)
-
-interesting_content = []
-for section, sentences in section_sentences.items():
-	orderer	= section_sentence_orderer(section)
-	index	= 1.0
-
-	for sentence in sentences:
-		position	= index / len(sentences)
-		order		= orderer(section=section, position=position)
-		interesting_content.append(sentence)
-		print(str(order) + " - " + sentence)
-		index = index + 1
+			position	= index / len(sentences)
+			order		= orderer(section=section, position=position)
+			index		= index + 1
+			interesting_content.append((sentence, order))
 
 
-if len(interesting_content) is not 0:
-	print choice(interesting_content)
+	if len(interesting_content) is not 0:
+		(sentence, order)	= choice(interesting_content)
+		novel_length		= novel_length + len(sentence)
+		novel.append((sentence, order))
+
+		# wikipedia doesn't have a rate limit, but whatever, let's do 'em a solid
+		time.sleep(0.1)
+
+ordered_novel = sorted(novel, key=lambda whatever: whatever[1])
+for (sentence, _) in ordered_novel:
+	print sentence
