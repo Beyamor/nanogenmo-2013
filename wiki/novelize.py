6a4646436e89a8aea969e7ca6b24cde1cecbb50d
beyamor <beyamor@gmail.com>
2013-11-11 22:55:06 -0800

    Removed the ordering stuff

--- wiki/novelize.py
+++ wiki/novelize.py
@@ -13,26 +13,11 @@ opts_parser.add_option("-w", "--words", dest="desired_length", type="int", defau
 opts_parser.add_option("-f", "--f", dest="novel_name")
 (options, _) = opts_parser.parse_args()
 
-def constantly(value):
-	def f(**kwargs):
-		return value
-	return f
-
-def positional_order(position=0, **kwargs):
-	return position
-
-def life_order(section="", **kwargs):
-	if "early" in section:
-		return 0
-
-	else:
-		return positional_order(**kwargs)
-
-SECTION_SENTENCE_ORDERERS = {
-		None:		positional_order,
-		"life":		life_order,
-		"death":	constantly(1),
-		"biography":	positional_order
+INTERESTING_SECTION_TERMS = {
+		None,
+		"life",
+		"death",
+		"biography"
 }
 
 def is_interesting_section(section):
@@ -40,21 +25,10 @@ def is_interesting_section(section):
 		return True
 
 	canonical_section = section.lower()
-	for term in SECTION_SENTENCE_ORDERERS:
+	for term in INTERESTING_SECTION_TERMS:
 		if term and term in canonical_section:
 			return True
 	return False
-
-def section_sentence_orderer(section):
-	if section is None:
-		return SECTION_SENTENCE_ORDERERS[None]
-	else:
-		canonical_section = section.lower()
-		for (term, orderer) in SECTION_SENTENCE_ORDERERS.items():
-			if term and term in canonical_section:
-				return orderer
-
-	raise Exception("No orderer for " + section)
 
 def parse_author_name(link):
 	name	= link[len("/wiki/"):]
@@ -74,7 +48,7 @@ while novel_length < options.desired_length:
 		link			= choice(author_links)
 		soup			= BeautifulSoup(urllib2.urlopen("http://en.wikipedia.org" + link).read().decode("utf-8"))
 		content			= soup.find(id="mw-content-text")
-		section_sentences	= {}
+		interesting_sentences	= []
 		current_section		= None
 		author_name		= parse_author_name(link)
 
@@ -86,10 +60,6 @@ while novel_length < options.desired_length:
 					current_section = section_heading.get_text()
 
 			elif content_item.name == "p" and is_interesting_section(current_section):
-				if not current_section in section_sentences:
-					section_sentences[current_section] = []
-				exiting_sentences = section_sentences[current_section]
-
 				sentences = nltk.tokenize.sent_tokenize(content_item.get_text())
 				for content in sentences:
 					content	= re.sub(r"\[\d+\]", "", content)
@@ -110,26 +80,13 @@ while novel_length < options.desired_length:
 									contains_author_name = True
 									break
 
-						exiting_sentences.append({
+						interesting_sentences.append({
 							"content": content,
 							"contains-author-name": contains_author_name
 						})
 
-		interesting_content = []
-		for section, sentences in section_sentences.items():
-			orderer	= section_sentence_orderer(section)
-			index	= 1.0
-
-			for sentence in sentences:
-				position		= index / len(sentences)
-				order			= orderer(section=section, position=position)
-				index			= index + 1
-				sentence["order"]	= order
-				interesting_content.append(sentence)
-
-
-		if len(interesting_content) is not 0:
-			sentence	= choice(interesting_content)
+		if len(interesting_sentences) is not 0:
+			sentence	= choice(interesting_sentences)
 			number_of_words	= sentence["content"].count(" ") + 1 # whatevs, close enough
 			novel_length	= novel_length + number_of_words
 			novel.append(sentence)
@@ -137,26 +94,25 @@ while novel_length < options.desired_length:
 			# wikipedia doesn't have a rate limit, but hey, let's not push it
 			time.sleep(0.1)
 
-	except:
-		pass
+	except e:
+		print e
 
 if options.novel_name is not None:
 	novel_name = options.novel_name
 else:
 	novel_name = "novel-" + datetime.datetime.now().strftime("%A-%d-%B-%Y-%I:%M%p")
 
-ordered_novel		= sorted(novel, key=lambda sentence: sentence["order"])
 continuing_sentence	= r"(He|She|His|Her).*"
 is_first_sentence	= True
-with open(novel_name, "w") as novel:
-	for sentence in ordered_novel:
+with open(novel_name, "w") as novel_file:
+	for sentence in novel:
 		content = sentence["content"]
 
 		if not is_first_sentence:
 			if sentence["contains-author-name"]:
-				novel.write("\n\n")
+				novel_file.write("\n\n")
 			else:
-				novel.write(" ")
+				novel_file.write(" ")
 
-		novel.write(content.encode("utf-8"))
+		novel_file.write(content.encode("utf-8"))
 		is_first_sentence = False
